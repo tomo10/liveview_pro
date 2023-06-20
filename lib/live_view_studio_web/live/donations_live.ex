63b177bf0b43b9a532a4552194fe119202486d11
tomo <thce28@gmail.com>
2023-06-20 11:48:08 +0100

    Add video content of donations

--- lib/live_view_studio_web/live/donations_live.ex
+++ lib/live_view_studio_web/live/donations_live.ex
@@ -3,14 +3,43 @@ defmodule LiveViewStudioWeb.DonationsLive do
 
   alias LiveViewStudio.Donations
 
-  def mount(_params, _session, socket) do
-    donations = Donations.list_donations()
+  def mount(_params, _session, socket), do: {:ok, socket}
+
+  def handle_params(params, _uri, socket) do
+    # list_donation expects a map with the keys as atoms. So need to convert
+    sort_by = (params["sort_by"] || "id") |> String.to_atom()
+    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+
+    options = %{
+      sort_by: sort_by,
+      sort_order: sort_order
+    }
+
+    donations = Donations.list_donations(options)
 
     socket =
       assign(socket,
-        donations: donations
+        donations: donations,
+        options: options
       )
 
-    {:ok, socket}
+    {:noreply, socket}
+  end
+
+  def sort_link(assigns) do
+    ~H"""
+    <.link patch={
+      ~p"/donations?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"
+    }>
+      <%= render_slot(@inner_block) %>
+    </.link>
+    """
+  end
+
+  defp next_sort_order(sort_order) do
+    case sort_order do
+      :asc -> :desc
+      :desc -> :asc
+    end
   end
 end
