aa9757bc146a5682cd9fdccffe2bf67f8e89fcec
tomo <thce28@gmail.com>
2023-06-20 13:26:25 +0100

    Add diy pizza example working

--- lib/live_view_studio_web/live/donations_live.ex
+++ lib/live_view_studio_web/live/donations_live.ex
@@ -8,7 +8,8 @@ defmodule LiveViewStudioWeb.DonationsLive do
   def handle_params(params, _uri, socket) do
     # list_donation expects a map with the keys as atoms. So need to convert
     sort_by = (params["sort_by"] || "id") |> String.to_atom()
-    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+    # sort order has better way of validating params
+    sort_order = valid_sort_order(params)
 
     options = %{
       sort_by: sort_by,
@@ -26,20 +27,42 @@ defmodule LiveViewStudioWeb.DonationsLive do
     {:noreply, socket}
   end
 
+  attr :sort_by, :atom, required: true
+  attr :options, :map, required: true
+  slot :inner_block, required: true
+
   def sort_link(assigns) do
     ~H"""
     <.link patch={
       ~p"/donations?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"
     }>
       <%= render_slot(@inner_block) %>
+      <%= sort_indicator(@sort_by, @options) %>
     </.link>
     """
   end
 
-  defp next_sort_order(sort_order) do
+  def next_sort_order(sort_order) do
     case sort_order do
       :asc -> :desc
       :desc -> :asc
     end
   end
+
+  def valid_sort_order(%{"sort_order" => sort_order})
+      when sort_order in ~w(asc desc) do
+    String.to_atom(sort_order)
+  end
+
+  def valid_sort_order(_), do: :desc
+
+  defp sort_indicator(column, %{sort_by: sort_by, sort_order: sort_order})
+       when column == sort_by do
+    case sort_order do
+      :asc -> "▲"
+      :desc -> "▼"
+    end
+  end
+
+  defp sort_indicator(_column, _options), do: ""
 end
--- lib/live_view_studio_web/live/pizza_orders_live.ex
+++ lib/live_view_studio_web/live/pizza_orders_live.ex
@@ -2,14 +2,44 @@ defmodule LiveViewStudioWeb.PizzaOrdersLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.PizzaOrders
+  alias LiveViewStudioWeb.DonationsLive
   import Number.Currency
 
   def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  def handle_params(params, _uri, socket) do
+    sort_by = (params["sort_by"] || "id") |> String.to_existing_atom()
+    sort_order = DonationsLive.valid_sort_order(params)
+
+    options = %{
+      sort_by: sort_by,
+      sort_order: sort_order
+    }
+
+    pizza_orders = PizzaOrders.list_pizza_orders(options)
+
     socket =
       assign(socket,
-        pizza_orders: PizzaOrders.list_pizza_orders()
+        pizza_orders: pizza_orders,
+        options: options
       )
 
-    {:ok, socket}
+    {:noreply, socket}
+  end
+
+  attr :sort_by, :atom, required: true
+  attr :options, :map, required: true
+  slot :inner_block, required: true
+
+  def sort_link(assigns) do
+    ~H"""
+    <.link patch={
+      ~p"/pizza-orders?#{%{sort_by: assigns.sort_by, sort_order: DonationsLive.next_sort_order(assigns.options.sort_order)}}"
+    }>
+      <%= render_slot(@inner_block) %>
+    </.link>
+    """
   end
 end
