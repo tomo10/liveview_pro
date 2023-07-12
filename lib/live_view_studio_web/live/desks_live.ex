22cce24537482f445419daa181d6f392e2072e3a
tomo <thce28@gmail.com>
2023-07-12 12:44:09 +0100

    Copy images to local server from to priv/static/uploads

--- lib/live_view_studio_web/live/desks_live.ex
+++ lib/live_view_studio_web/live/desks_live.ex
@@ -38,6 +38,31 @@ defmodule LiveViewStudioWeb.DesksLive do
   end
 
   def handle_event("save", %{"desk" => params}, socket) do
+    # copy temp tile to priv/static/uploads/abc-1.png
+    # URL path: /uploads/abc-1.png
+
+    # list of url paths, one for each entry
+    photo_locations =
+      consume_uploaded_entries(socket, :photos, fn meta, entry ->
+        dest =
+          Path.join([
+            "priv",
+            "static",
+            "uploads",
+            "#{entry.uuid}-#{entry.client_name}"
+          ])
+
+        File.mkdir_p!(Path.dirname(dest))
+
+        File.cp!(meta.path, dest)
+
+        url_path = static_path(socket, "/uploads/#{Path.basename(dest)}")
+
+        {:ok, url_path}
+      end)
+
+    params = Map.put(params, "photo_locations", photo_locations)
+
     case Desks.create_desk(params) do
       {:ok, _desk} ->
         changeset = Desks.change_desk(%Desk{})
